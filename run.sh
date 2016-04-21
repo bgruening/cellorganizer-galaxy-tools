63091e839c2f1387916d0ac14d58c68999b8c3a8
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2016-04-21 17:45:33 +0200

    Updated requirements file from virtualenv to include the packages we use in this repo.

--- run.sh
+++ run.sh
@@ -4,5 +4,7 @@ cp galaxy.ini galaxy/config
 cp tool_conf.xml galaxy/config
 rsync -ruv tools/ galaxy/tools/
 
+cp requirements.txt ./galaxy/lib/galaxy/dependencies/requirements.txt 
+
 cd galaxy
 ./run.sh
