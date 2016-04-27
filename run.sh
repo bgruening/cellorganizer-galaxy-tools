7856a9f561cbfa0e1947ee1451a74efe3037829a
Yanyu Liang <yanyul@andrew.cmu.edu>
2016-04-27 11:26:58 -0400

    slightly changed run.sh, where data_manager_conf.xml is deleted

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ cp galaxy.ini galaxy/config
 cp tool_conf.xml galaxy/config
 cp datatypes_conf.xml galaxy/config
 cp requirements.txt galaxy/requirements.txt
-cp data_manager_conf.xml galaxy/config/data_manager_conf.xml
 cp -r data galaxy/
 cp tool_data_table_conf.xml galaxy/config/tool_data_table_conf.xml
 rsync -ruv datatypes/ galaxy/lib/galaxy/datatypes/
