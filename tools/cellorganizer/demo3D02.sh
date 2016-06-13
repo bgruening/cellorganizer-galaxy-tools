014871e862d0c191a1f62a4cc719a9b39a0903fb
Nicole Park <nicolepa@woodstock.compbio.cs.cmu.edu>
2016-06-13 14:07:06 -0700

    Shell script for demo3D02

--- tools/cellorganizer/demo3D02.sh
+++ tools/cellorganizer/demo3D02.sh
@@ -7,7 +7,50 @@ WORKING_DIRECTORY=`pwd`
 
 MATLAB=/opt/packages/matlab/R2016a/bin/matlab
 
+NAME=$1
+SEED=$2
+ALPHAVAL=$3
+VIEWANGLES1=$4
+VIEWANGLES2=$5
+
 echo "
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DO NOT MODIFY THIS BLOCK
+answer = false;
+current_path = which(mfilename);
+[current_path, filename, extension] = fileparts( current_path );
+cd(current_path);
+
+options.seed = 12345;
+try
+    state = rng( options.seed );
+catch err
+    state = rand( 'seed', options.seed ); %#ok<RAND>
+end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FEEL FREE TO MODIFY THE VARIABLES IN THIS BLOCK
+tiff_directory = '../demo3D00/synthesizedImages/cell1';
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+if ~exist( tiff_directory )
+    warning(['Directory ' tiff_directory ' not found. Run demo first.']);
+    return
+end
+
+try
+    colors = {'blue', 'green' 'red'};
+    viewangles = [20,-30];
+
+    %transparency
+    alphaval = 0.1;
+    syn2surfaceplot( tiff_directory, colors, viewangles, alphaval );
+    answer = true;
+catch
+    warning( 'Unable to make surface plot.' );
+end
+
 exit;" > script.m
 
 echo "Running the following script in Matlab"
