e4d400a7ccf12824fa65a20abc9904b4fbb4aba5
Nicole Matamala <nmatamal@andrew.cmu.edu>
2016-06-13 14:03:11 -0400

    Created the shell script for demo3D15 according to the XML file inputs (seed, compression).

---
+++ tools/cellorganizer/demo3D15.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+export PATH=$PATH:$(dirname $0)
+CELLORGANIZER=/pylon1/mc4s8dp/icaoberg/galaxy/cellorganizer
+
+WORKING_DIRECTORY=`pwd`
+
+MATLAB=/opt/packages/matlab/R2016a/bin/matlab
+
+SEED=$1
+COMPRESSION=$2
+
+echo "
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DO NOT MODIFY THIS BLOCK
+current_path = which(mfilename);
+[current_path, filename, extension] = fileparts( current_path );
+cd(current_path);
+
+options.seed = 12345;
+try
+    state = rng( options.seed );
+catch err
+    state = rand( 'seed', options.seed ); %#ok<RAND>
+end
+
+outputDirectory = pwd;
+options = [];
+options.targetDirectory = outputDirectory;
+options.prefix = 'synthesizedImages';
+options.numberOfSynthesizedImages = 1;
+options.compression = 'lzw';
+options.microscope = 'none';
+options.sampling.method = 'disc';
+options.verbose = true;
+options.debug = true;
+options.synthesis = 'framework';
+options.synthesis.diffeomorphic.maximum_iterations = 40;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% FEEL FREE TO MODIFY THE VARIABLES IN THIS BLOCK
+list_of_models = {'./diffeomorphic.mat'};
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+answer = slml2img( list_of_models, options );
+
+exit;" > script.m
+
+echo "Running the following script in Matlab"
+cat script.m
+
+echo $WORKING_DIRECTORY
+ln -s $CELLORGANIZER $(pwd)/cellorganizer
+$MATLAB -nodesktop -nosplash -r "script;"
+
+echo "Compressing results"
+zip -rv examples.zip examples
+rm -rfv examples
